d3e12223f4da25497aa3ef7af20b458b55e71522
duxetech <karthikriches@gmail.com>
2020-09-24 12:15:22 +0530

    Initial Commit

---
+++ Employee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employee
+//
+//  Created by Karthik on 9/24/20.
+//  Copyright © 2020 Karthik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
